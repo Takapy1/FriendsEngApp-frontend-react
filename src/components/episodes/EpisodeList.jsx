2a2abeac5043404150d20fa75fadd7cd0b7e37e8
Takapy1 <takahirog1109mai@gmail.com>
2022-02-01 12:29:56 +0900

    [fix] episodeList and SeasonList adding links

--- src/components/episodes/EpisodeList.jsx
+++ src/components/episodes/EpisodeList.jsx
@@ -1,8 +1,37 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
+import axios from 'axios'
+import { Link, useParams } from "react-router-dom";
 
-export const EpisodeList = () => { 
+export const EpisodeList = () => {
+  const [numEpisodes, setNumEpisodes] = useState();
+  const [links, setLinks] = useState([]);
+  const seasonId = useParams().id;
+  console.log(useParams);
+
+  useEffect( () => {
+    axios.get(process.env.REACT_APP_SERVER_URL + `/api/v1/seasons/${seasonId}/episodes`)
+    .then(res => {
+      console.log(res.data.data.length);
+      setNumEpisodes(res.data.data.length);
+      setLinks([...Array(numEpisodes).keys()]);
+    })
+    .catch(e => {
+      console.log(e);
+    });
+  }, [])
 
   return (
-    <><p>episode list page</p></>
+    <>
+      <h1>Season {seasonId}, Episode一覧</h1>
+      <div>
+        {links.map(val => {
+          return(
+            <div>
+              <Link to={`/seasons/1/episodes/${val+1}`}>Episode {val + 1}</Link>
+            </div> 
+          )
+        })}
+      </div>
+    </>
   )
 }
--- src/components/episodes/ShowEpisode.jsx
+++ src/components/episodes/ShowEpisode.jsx
@@ -1,6 +1,18 @@
-import React, { Component } from "react";
+import axios from "axios";
+import React, { Component, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 export const ShowEpisode = () => { 
+  console.log(useParams.id);
+  // const seasonId = useParams().id;
+
+  // useEffect( () => {
+  //   axios.get(process.env.REACT_APP_SERVER_URL + `/api/v1/seasons/${seasonId}/episodes/${episodeId}`)
+  // })
+
+  // const getLines () => {
+
+  // }
 
   return (
     <>
