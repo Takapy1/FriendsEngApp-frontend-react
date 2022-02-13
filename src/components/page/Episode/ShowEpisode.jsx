81d0a9a9f9688a9fe8ea8b8dc0af7edc97558d28
Ayato-kosaka <53715839+Ayato-kosaka@users.noreply.github.com>
2022-02-13 19:55:23 +0900

    Review ShowEpisode.jsx
    ShowEpisode のスコープは、下記2点に絞る。
    ①状態変数 ActiveLineID, ActiveWordIndex を管理する
    ②セリフの一覧取得
    ③Line コンポーネントの呼び出し, Sidebar コンポーネントの呼び出し
    
    Line コンポーネントのスコープは下記
    ①セリフの表示
    ②アクティブの処理（分割、setActiveLineID, setActiveWordIndex のキック）
    
    Sidebar コンポーネントのスコープは下記
    ①単語、例文を取得
    ②例文の登録

--- src/components/page/Episode/ShowEpisode.jsx
+++ src/components/page/Episode/ShowEpisode.jsx
@@ -17,7 +17,6 @@ export const ShowEpisode = () => {
   const episodeId = useParams().id;
   const [lines, setLines] = useState([]);
   const [activeLineID, setActiveLineID] = useState(null);
-  const [activeWord, setActiveWord] = useState(null);
   const [activeWordIndex, setActiveWordIndex] = useState(null);
   const [meaningList, setMeaningList] = useState([]);
 
@@ -31,59 +30,30 @@ export const ShowEpisode = () => {
     })
   }, []);
 
-  const handleClickLine = (id) => {
-    setActiveLineID(id);
-  }
-
-  const handleActiveWord = (word, index) => {
-    setActiveWord(word);
-    setActiveWordIndex(index);
-    // console.log(index);
-  }
-
-  const handleMeaningList = async(index) => {
-    await axios.get(process.env.REACT_APP_SERVER_URL + `/api/v1/lines/${activeLineID}/words/${index}`)
-    .then(res => {
-      console.log(res.data);
-      setMeaningList(res.data);
-    })
-    .catch(e => {
-      console.log(e);
-      setMeaningList([]);
-    })
-  }
-
-  const getLine = (v) => {
-    if (activeLineID === v.id) {
-      return <ActiveLine 
-                  lineID={v.id} 
-                  line={v.content} 
-                  onLineClick={(id) => handleClickLine(id)} 
-                  handleActiveWord={(word, i) => handleActiveWord(word, i)}
-                  handleMeaningList={(num) => handleMeaningList(num) } />
-    } else {
-      return <Line key={`line${v.id}`} id={`line${v.id}`} onClick={() => handleClickLine(v.id)}>{v.content}</Line>;
-    }
-  }
-
-  const addNewMeaning = (meaning) => {
-    setMeaningList(meaning);
-  }
-
   return (
     <>
       <h1>Season {seasonId}, Episode {episodeId}</h1>
       <Link to={`/seasons/${seasonId}/episodes`}>Episode一覧に戻る</Link>
       <Wrapper>
         <Main>
-          { lines.map(val => getLine(val)) }
+          { lines.map(v => 
+            <Line 
+                  lineID={v.id} 
+                  line={v.content} 
+                  activeFlag={activeLineID === v.id}
+                  //Lineコンポーネント内で、setActiveLineID, setActiveWordIndex をキックさせる
+      
+                  onLineClick={(id) => setActiveLineID(id)} 
+                  handleActiveWord={(word, i) => setActiveWordIndex(word, i)} />
+          )}
         </Main>
         <SideBar 
             activeLineID={activeLineID} 
-            activeWord={activeWord} 
+            //activeWord は SideBar コンポーネント内で取得する
             activeWordIndex={activeWordIndex} 
-            meaningList={meaningList}
-            addNewMeaning={(m) => addNewMeaning(m)} />
+            //meaningList は SideBar コンポーネント内で取得する
+            //addNerMeaning は SideBar コンポーネント内で実装する
+            />
       </Wrapper>
     </>
   )
